394126865d1041af6a88c0cb5879887d2c58c848
Reetika12 <53554378+Reetika12@users.noreply.github.com>
2024-07-04 09:39:08 +0530

    Update index.js
    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post('/api/create-user', (req, res) => {
 
   // Respond with a success message
   // res.status(200).json({ message: 'User created successfully' });
-  if(surname=="shetty")
+  if(displayName=="shetty")
   {
     return res.json({
        action:'Continue',
