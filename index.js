69df572f7c2b77da6e870b4bfde9aebe994728e3
Reetika12 <53554378+Reetika12@users.noreply.github.com>
2024-07-04 09:33:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,18 @@ app.post('/api/create-user', (req, res) => {
 
   // Respond with a success message
   // res.status(200).json({ message: 'User created successfully' });
+  if(surname=="shetty")
+  {
+    return {
+      res.json({
+       action:'',
+       userMessage: "User is created Successfully! Go with MFA"
+    })
+    }
+  }
    return res.json({
-       action:'ShowBlockPage',
-       userMessage: "There was a problem with your request. You are not able to sign up at this time. Please contact your system Administrator!"
+       action:'Continue',
+       userMessage: "User is created Successfully! Go with MFA"
     })
 });
 // POST endpoint to include data for user before sending the token 
