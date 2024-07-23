3b4a651c15059a22b063efe1748a6b14d0b918cb
Reetika12 <53554378+Reetika12@users.noreply.github.com>
2024-07-23 16:06:12 +0530

    Update index.js
    updated block message

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post('/api/create-user', (req, res) => {
   // }
    return res.json({
       action:'ShowBlockPage',
-       userMessage: "There was a problem with your request. You are not able to sign up at this time. Please contact your system people!"
+      userMessage: "There was a problem with your request. You are not able to sign up at this time.There was a problem with your request.Please contact your system Administrator at 0800 198 4895"
     })
 });
 // POST endpoint to include data for user before sending the token 
