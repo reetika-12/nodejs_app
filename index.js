0dcde9ed5c28e7d4f4336aace5c23e3764006116
reetika-12 <168195965+reetika-12@users.noreply.github.com>
2024-08-02 20:08:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.post('/api/create-user', (req, res) => {
   const { email, identities, displayName, objectId, givenName, surname, jobTitle, streetAddress, city, postalCode, state, country, extension_customAttribute1, extension_customAttribute2, step, client_id, ui_locales } = req.body;
 
   // Log the received data (for demonstration purposes)
+  console.log("req before creating the user",req);
   console.log('Received request body:', req.body);
 
   // Example of handling the received data
